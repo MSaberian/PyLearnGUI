6726e8ebe87fb26f2a141247cb03b3eea0f89b33
MSaberian <mo.saberian@yahoo.com>
2023-02-02 19:28:34 +0330

    check winner

--- PyLearnGUITicTacToe/main.py
+++ PyLearnGUITicTacToe/main.py
@@ -3,24 +3,82 @@ from functools import partial
 from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtUiTools import QUiLoader
 
-def check():
-    if buttons[0][0].text() == 'X' and buttons[1][0].text() == 'X' and buttons[2][0].text() == 'X':
-        msg_box = QMessageBox(text= 'player 1 is wined')
-        msg_box.exec()
+player1_points = []
+player2_points = []
+CPU_number = -1
+
+def reset_page():
+    for row in range(3):
+        for col in range(3):
+            buttons[row][col].setText('')
+            buttons[row][col].setStyleSheet('')
+            player1_points.clear()
+            player2_points.clear()
+
+
+def check_game(player_points, player):
+    sum2 = 0
+    sum3 = 0
+    for i in range(3):
+        sum0 = 0
+        sum1 = 0
+        for j in range(3):
+            if [i,j] in player_points:
+                sum0 += 1
+            if [j,i] in player_points:
+                sum1 += 1
+
+        if [i,i] in player_points:
+            sum2 += 1
+        if [i,2-i] in player_points:
+            sum3 += 1
+        
+        if sum0 == 3 or sum1 == 3 or sum2 == 3 or sum3 == 3:
+            msg_box = QMessageBox(text= f'Player {player} is wined')
+            msg_box.exec()
+            reset_page()
+            return True
+    
+    return False
+
+def get_point(player_points, player_name):
+    while True:
+        if player_name == "CPU player":
+            row = random.randint(0, 2)
+            col = random.randint(0, 2)
+        else:
+            row = int(input("row: "))
+            col = int(input("col: "))
+
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            if not [row,col] in player1_points and not [row,col] in player2_points:
+                player_points.append([row,col])
+                break
+            elif player_name != "CPU player":
+                print("⚠ Don't enter repation point")
+        else:
+            print("⚠ you have to enter numbers between 0-2")
 
 def play(row, col):
     global player
-    if player == 1:
-        buttons[row][col].setText('X')
-        buttons[row][col].setStyleSheet('color: red; background-color: pink')
-        player = 2
 
-    elif player == 2:
-        buttons[row][col].setText('O')
-        buttons[row][col].setStyleSheet('color: blue; background-color: lightblue')
-        player = 1
-    
-    check()
+
+    if not [row,col] in player1_points and not [row,col] in player2_points:
+        if player == 1:
+            buttons[row][col].setText('X')
+            buttons[row][col].setStyleSheet('color: red; background-color: pink')
+            player1_points.append([row,col])
+            player = 2
+            check_game(player1_points, 'player 1')
+
+        elif player == 2:
+            buttons[row][col].setText('O')
+            buttons[row][col].setStyleSheet('color: blue; background-color: lightblue')
+            player2_points.append([row,col])
+            player = 1
+            check_game(player2_points, 'player 2')
+    else:
+        print('fault')
 
 
 app = QApplication(sys.argv)
--- PyLearnGUITicTacToe/ui_main.py
+++ PyLearnGUITicTacToe/ui_main.py
@@ -15,14 +15,15 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
+    QMenuBar, QPlainTextEdit, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(474, 310)
+        MainWindow.resize(489, 444)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -30,90 +31,117 @@ class Ui_MainWindow(object):
         MainWindow.setSizePolicy(sizePolicy)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.btn2 = QPushButton(self.centralwidget)
-        self.btn2.setObjectName(u"btn2")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.textbox_4 = QLineEdit(self.centralwidget)
+        self.textbox_4.setObjectName(u"textbox_4")
+        self.textbox_4.setGeometry(QRect(180, 310, 91, 41))
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.btn2.sizePolicy().hasHeightForWidth())
-        self.btn2.setSizePolicy(sizePolicy1)
+        sizePolicy1.setHeightForWidth(self.textbox_4.sizePolicy().hasHeightForWidth())
+        self.textbox_4.setSizePolicy(sizePolicy1)
         font = QFont()
-        font.setPointSize(40)
+        font.setPointSize(30)
         font.setBold(True)
-        self.btn2.setFont(font)
+        self.textbox_4.setFont(font)
+        self.textbox_4.setStyleSheet(u"background-color:rgb(170,170, 170);")
+        self.textbox_4.setReadOnly(False)
+        self.textbox = QLineEdit(self.centralwidget)
+        self.textbox.setObjectName(u"textbox")
+        self.textbox.setGeometry(QRect(180, 280, 61, 21))
+        sizePolicy1.setHeightForWidth(self.textbox.sizePolicy().hasHeightForWidth())
+        self.textbox.setSizePolicy(sizePolicy1)
+        self.textbox.setFont(font)
+        self.textbox.setStyleSheet(u"background-color:rgb(170,170, 170);")
+        self.textbox.setReadOnly(False)
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.btn1 = QPushButton(self.centralwidget)
+        self.btn1.setObjectName(u"btn1")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.btn1.sizePolicy().hasHeightForWidth())
+        self.btn1.setSizePolicy(sizePolicy2)
+        font1 = QFont()
+        font1.setPointSize(40)
+        font1.setBold(True)
+        self.btn1.setFont(font1)
 
-        self.gridLayout.addWidget(self.btn2, 0, 3, 1, 1)
+        self.gridLayout.addWidget(self.btn1, 0, 0, 1, 1)
+
+        self.btn2 = QPushButton(self.centralwidget)
+        self.btn2.setObjectName(u"btn2")
+        sizePolicy2.setHeightForWidth(self.btn2.sizePolicy().hasHeightForWidth())
+        self.btn2.setSizePolicy(sizePolicy2)
+        self.btn2.setFont(font1)
+
+        self.gridLayout.addWidget(self.btn2, 0, 1, 1, 1)
 
         self.btn3 = QPushButton(self.centralwidget)
         self.btn3.setObjectName(u"btn3")
-        sizePolicy1.setHeightForWidth(self.btn3.sizePolicy().hasHeightForWidth())
-        self.btn3.setSizePolicy(sizePolicy1)
-        self.btn3.setFont(font)
+        sizePolicy2.setHeightForWidth(self.btn3.sizePolicy().hasHeightForWidth())
+        self.btn3.setSizePolicy(sizePolicy2)
+        self.btn3.setFont(font1)
 
-        self.gridLayout.addWidget(self.btn3, 0, 5, 1, 1)
-
-        self.btn1 = QPushButton(self.centralwidget)
-        self.btn1.setObjectName(u"btn1")
-        sizePolicy1.setHeightForWidth(self.btn1.sizePolicy().hasHeightForWidth())
-        self.btn1.setSizePolicy(sizePolicy1)
-        self.btn1.setFont(font)
-
-        self.gridLayout.addWidget(self.btn1, 0, 1, 1, 1)
-
-        self.btn5 = QPushButton(self.centralwidget)
-        self.btn5.setObjectName(u"btn5")
-        sizePolicy1.setHeightForWidth(self.btn5.sizePolicy().hasHeightForWidth())
-        self.btn5.setSizePolicy(sizePolicy1)
-        self.btn5.setFont(font)
-
-        self.gridLayout.addWidget(self.btn5, 1, 3, 1, 1)
+        self.gridLayout.addWidget(self.btn3, 0, 2, 1, 1)
 
         self.btn4 = QPushButton(self.centralwidget)
         self.btn4.setObjectName(u"btn4")
-        sizePolicy1.setHeightForWidth(self.btn4.sizePolicy().hasHeightForWidth())
-        self.btn4.setSizePolicy(sizePolicy1)
-        self.btn4.setFont(font)
+        sizePolicy2.setHeightForWidth(self.btn4.sizePolicy().hasHeightForWidth())
+        self.btn4.setSizePolicy(sizePolicy2)
+        self.btn4.setFont(font1)
 
-        self.gridLayout.addWidget(self.btn4, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.btn4, 1, 0, 1, 1)
 
-        self.btn7 = QPushButton(self.centralwidget)
-        self.btn7.setObjectName(u"btn7")
-        sizePolicy1.setHeightForWidth(self.btn7.sizePolicy().hasHeightForWidth())
-        self.btn7.setSizePolicy(sizePolicy1)
-        self.btn7.setFont(font)
+        self.btn5 = QPushButton(self.centralwidget)
+        self.btn5.setObjectName(u"btn5")
+        sizePolicy2.setHeightForWidth(self.btn5.sizePolicy().hasHeightForWidth())
+        self.btn5.setSizePolicy(sizePolicy2)
+        self.btn5.setFont(font1)
 
-        self.gridLayout.addWidget(self.btn7, 2, 1, 1, 1)
-
-        self.btn8 = QPushButton(self.centralwidget)
-        self.btn8.setObjectName(u"btn8")
-        sizePolicy1.setHeightForWidth(self.btn8.sizePolicy().hasHeightForWidth())
-        self.btn8.setSizePolicy(sizePolicy1)
-        self.btn8.setFont(font)
-
-        self.gridLayout.addWidget(self.btn8, 2, 3, 1, 1)
-
-        self.btn9 = QPushButton(self.centralwidget)
-        self.btn9.setObjectName(u"btn9")
-        sizePolicy1.setHeightForWidth(self.btn9.sizePolicy().hasHeightForWidth())
-        self.btn9.setSizePolicy(sizePolicy1)
-        self.btn9.setFont(font)
-
-        self.gridLayout.addWidget(self.btn9, 2, 5, 1, 1)
+        self.gridLayout.addWidget(self.btn5, 1, 1, 1, 1)
 
         self.btn6 = QPushButton(self.centralwidget)
         self.btn6.setObjectName(u"btn6")
-        sizePolicy1.setHeightForWidth(self.btn6.sizePolicy().hasHeightForWidth())
-        self.btn6.setSizePolicy(sizePolicy1)
-        self.btn6.setFont(font)
+        sizePolicy2.setHeightForWidth(self.btn6.sizePolicy().hasHeightForWidth())
+        self.btn6.setSizePolicy(sizePolicy2)
+        self.btn6.setFont(font1)
 
-        self.gridLayout.addWidget(self.btn6, 1, 5, 1, 1)
+        self.gridLayout.addWidget(self.btn6, 1, 2, 1, 1)
+
+        self.btn7 = QPushButton(self.centralwidget)
+        self.btn7.setObjectName(u"btn7")
+        sizePolicy2.setHeightForWidth(self.btn7.sizePolicy().hasHeightForWidth())
+        self.btn7.setSizePolicy(sizePolicy2)
+        self.btn7.setFont(font1)
+
+        self.gridLayout.addWidget(self.btn7, 2, 0, 1, 1)
+
+        self.btn8 = QPushButton(self.centralwidget)
+        self.btn8.setObjectName(u"btn8")
+        sizePolicy2.setHeightForWidth(self.btn8.sizePolicy().hasHeightForWidth())
+        self.btn8.setSizePolicy(sizePolicy2)
+        self.btn8.setFont(font1)
+
+        self.gridLayout.addWidget(self.btn8, 2, 1, 1, 1)
+
+        self.btn9 = QPushButton(self.centralwidget)
+        self.btn9.setObjectName(u"btn9")
+        sizePolicy2.setHeightForWidth(self.btn9.sizePolicy().hasHeightForWidth())
+        self.btn9.setSizePolicy(sizePolicy2)
+        self.btn9.setFont(font1)
+
+        self.gridLayout.addWidget(self.btn9, 2, 2, 1, 1)
+
+        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+
+        self.gridLayout.addWidget(self.plainTextEdit, 3, 1, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 474, 22))
+        self.menubar.setGeometry(QRect(0, 0, 489, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -126,14 +154,16 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.textbox_4.setText("")
+        self.textbox.setText("")
+        self.btn1.setText("")
         self.btn2.setText("")
         self.btn3.setText("")
-        self.btn1.setText("")
-        self.btn5.setText("")
         self.btn4.setText("")
+        self.btn5.setText("")
+        self.btn6.setText("")
         self.btn7.setText("")
         self.btn8.setText("")
         self.btn9.setText("")
-        self.btn6.setText("")
     # retranslateUi
 
