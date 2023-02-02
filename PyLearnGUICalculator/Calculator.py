bbf8f1d6d90962bc8a473545e4f00fdfe1bb8f21
MSaberian <mo.saberian@yahoo.com>
2023-02-02 11:23:41 +0330

    final

--- PyLearnGUICalculator/Calculator.py
+++ PyLearnGUICalculator/Calculator.py
@@ -1,94 +1,74 @@
-
 from functools import partial
 import math
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 
-def sum():
-    global a
-    global state
-    state = 'sum'
-    a = float(main_window.textbox.text())
-    main_window.textbox.setText('')
-    
-def operation(op):
+def operation_2part(op):
     global a
     global state
     state = op
     a = float(main_window.textbox.text())
     main_window.textbox.setText('')
 
-def sub():
-    global a
-    global state
-    state = 'sub'
-    a = float(main_window.textbox.text())
-    main_window.textbox.setText('')
-    
-def mul():
-    global a
-    global state
-    state = 'mul'
-    a = float(main_window.textbox.text())
-    main_window.textbox.setText('')
-    
-def dev():
-    global a
-    global state
-    state = 'dev'
-    a = float(main_window.textbox.text())
-    main_window.textbox.setText('')
-    
-def AC():
-    main_window.textbox.setText('')
-    
-def per():
-    main_window.textbox.setText(str(float(main_window.textbox.text())/100))
-    
-def PN():
-    main_window.textbox.setText(str(float(main_window.textbox.text())*(-1)))
+def operation(op):
+    number = float(main_window.textbox.text())
+    if op == 'AC':
+        number = 0
+    elif op == 'per':
+        number = number/100
+    elif op == 'PN':
+        number = -1*number
+    elif op == 'sin':
+        number = math.sin(number)
+    elif op == 'cos':
+        number = math.cos(number)
+    elif op == 'tan':
+        number = math.tan(number)
+    elif op == 'cot':
+        number = 1/math.tan(number)
+    elif op == 'log':
+        number = math.log(number)
+    elif op == 'sqrt':
+        number = math.sqrt(number)
 
-def sin():
-    main_window.textbox.setText(str(math.sin(float(main_window.textbox.text()))))
-    
-def cos():
-    main_window.textbox.setText(str(math.cos(float(main_window.textbox.text()))))
-    
-def tan():
-    main_window.textbox.setText(str(math.tan(float(main_window.textbox.text()))))
-    
-def cot():
-    main_window.textbox.setText(str(1/math.tan(float(main_window.textbox.text()))))
-    
-def log():
-    main_window.textbox.setText(str(math.log(float(main_window.textbox.text()))))
-    
-def sqrt():
-    main_window.textbox.setText(str(math.sqrt(float(main_window.textbox.text()))))
+    main_window.textbox.setText(str(number))
         
 def n(x):
+    global equal_pressed
+    if equal_pressed:
+        equal_pressed = False
+        main_window.textbox.setText('')
+    if main_window.textbox.text() == '0':
+        main_window.textbox.setText('')
     main_window.textbox.setText(main_window.textbox.text()+x)
     
 def dot():
+    global equal_pressed
+    if equal_pressed:
+        equal_pressed = False
+        main_window.textbox.setText('')
     main_window.textbox.setText(main_window.textbox.text()+'.')
 
 def result():
     global b
+    global equal_pressed
+    equal_pressed = True
     b = float(main_window.textbox.text())
-    if state in globals():
-        if state == 'sum':
-            c = a + b
-        elif state == 'sub':
-            c = a - b
-        elif state == 'mul':
-            c = a * b
-        elif state == 'dev':
-            c = a / b
-        else:
-            c = 0
+    if state == 'sum':
+        c = a + b
+    elif state == 'sub':
+        c = a - b
+    elif state == 'mul':
+        c = a * b
+    elif state == 'dev':
+        c = a / b
+    else:
+        c = 0
     
     main_window.textbox.setText(str(c))
 
+
+equal_pressed = False
 
 my_app = QApplication([])
 
@@ -96,19 +76,19 @@ loader = QUiLoader()
 main_window = loader.load('main.ui')
 main_window.show()
 
-main_window.sum.clicked.connect(partial(operation,'sum'))
-main_window.sub.clicked.connect(partial(operation,'sub'))
-main_window.mul.clicked.connect(partial(operation,'mul'))
-main_window.dev.clicked.connect(partial(operation,'dev'))
-main_window.AC.clicked.connect(AC)
-main_window.percent.clicked.connect(per)
-main_window.PN.clicked.connect(PN)
-main_window.sin.clicked.connect(sin)
-main_window.cos.clicked.connect(cos)
-main_window.tan.clicked.connect(tan)
-main_window.cot.clicked.connect(cot)
-main_window.log.clicked.connect(log)
-main_window.sqrt.clicked.connect(sqrt)
+main_window.sum.clicked.connect(partial(operation_2part,'sum'))
+main_window.sub.clicked.connect(partial(operation_2part,'sub'))
+main_window.mul.clicked.connect(partial(operation_2part,'mul'))
+main_window.dev.clicked.connect(partial(operation_2part,'dev'))
+main_window.AC.clicked.connect(partial(operation,'AC'))
+main_window.percent.clicked.connect(partial(operation,'per'))
+main_window.PN.clicked.connect(partial(operation,'PN'))
+main_window.sin.clicked.connect(partial(operation,'sin'))
+main_window.cos.clicked.connect(partial(operation,'cos'))
+main_window.tan.clicked.connect(partial(operation,'tan'))
+main_window.cot.clicked.connect(partial(operation,'cot'))
+main_window.log.clicked.connect(partial(operation,'log'))
+main_window.sqrt.clicked.connect(partial(operation,'sqrt'))
 main_window.n1.clicked.connect(partial(n,'1'))
 main_window.n2.clicked.connect(partial(n,'2'))
 main_window.n3.clicked.connect(partial(n,'3'))
