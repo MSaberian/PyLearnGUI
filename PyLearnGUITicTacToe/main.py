d4b06eaa264dacc6da9afda8301d39a9991a55b9
MSaberian <mo.saberian@yahoo.com>
2023-02-03 14:13:18 +0330

    final game

--- PyLearnGUITicTacToe/main.py
+++ PyLearnGUITicTacToe/main.py
@@ -1,21 +1,37 @@
 import sys
+from random import randint
 from functools import partial
 from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtUiTools import QUiLoader
 
 player1_points = []
 player2_points = []
-CPU_number = -1
+
+def set_scroreboard_color():
+    if player == 1:
+        main_window.gb_x.setStyleSheet('background-color: pink')
+        main_window.gb_o.setStyleSheet('background-color: gray')
+    else:
+        main_window.gb_o.setStyleSheet('background-color: lightblue')
+        main_window.gb_x.setStyleSheet('background-color: gray')
 
 def reset_page():
+    global player
+    player = randint(1, 2)
+
     for row in range(3):
         for col in range(3):
             buttons[row][col].setText('')
             buttons[row][col].setStyleSheet('')
             player1_points.clear()
             player2_points.clear()
+            
+    if main_window.rb_cpu.isChecked() and player == 2:
+        add_cpu_point()
+        player = 1
+    set_scroreboard_color()
 
-def check_game(player_points, player):
+def check_game(player_points, player_name):
     sum2 = 0
     sum3 = 0
     for i in range(3):
@@ -33,7 +49,7 @@ def check_game(player_points, player):
             sum3 += 1
         
         if sum0 == 3 or sum1 == 3 or sum2 == 3 or sum3 == 3:
-            msg_box = QMessageBox(text= f'Player {player} is wined')
+            msg_box = QMessageBox(text= f'{player_name} is wined')
             msg_box.exec()
             reset_page()
             return True
@@ -43,8 +59,8 @@ def check_game(player_points, player):
 def get_point(player_points, player_name):
     while True:
         if player_name == "CPU player":
-            row = random.randint(0, 2)
-            col = random.randint(0, 2)
+            row = randint(0, 2)
+            col = randint(0, 2)
         else:
             row = int(input("row: "))
             col = int(input("col: "))
@@ -58,35 +74,78 @@ def get_point(player_points, player_name):
         else:
             print("⚠ you have to enter numbers between 0-2")
 
+def add_cpu_point():
+    while True:
+        row = randint(0, 2)
+        col = randint(0, 2)
+        if not [row,col] in player1_points and not [row,col] in player2_points:
+            buttons[row][col].setText('O')
+            buttons[row][col].setStyleSheet('color: blue; background-color: lightblue')
+            player2_points.append([row,col])
+            if check_game(player2_points, 'CPU'):
+                main_window.tb_o.setText(str(int(main_window.tb_o.text())+1))
+            break
+
+def check_draw():
+    if len(player1_points) + len(player2_points) == 9:
+        msg_box = QMessageBox(text= 'ِDraw!!')
+        main_window.tb_t.setText(str(int(main_window.tb_t.text())+1))
+        msg_box.exec()
+        reset_page()
+        return True
+
 def play(row, col):
     global player
-
-
     if not [row,col] in player1_points and not [row,col] in player2_points:
         if player == 1:
             buttons[row][col].setText('X')
             buttons[row][col].setStyleSheet('color: red; background-color: pink')
             player1_points.append([row,col])
-            player = 2
-            check_game(player1_points, 'player 1')
+            if check_game(player1_points, 'player 1'):
+                main_window.tb_x.setText(str(int(main_window.tb_x.text())+1))
+                return
+            if main_window.rb_cpu.isChecked():
+                if not check_draw():
+                    add_cpu_point()
+            else:
+                player = 2
 
         elif player == 2:
             buttons[row][col].setText('O')
             buttons[row][col].setStyleSheet('color: blue; background-color: lightblue')
             player2_points.append([row,col])
             player = 1
-            check_game(player2_points, 'player 2')
+            if check_game(player2_points, 'player 2'):
+                main_window.tb_o.setText(str(int(main_window.tb_o.text())+1))
     else:
         print('fault')
-
+    set_scroreboard_color()
+    check_draw()
+        
+def reset_game():
+    main_window.tb_t.setText('0')
+    main_window.tb_x.setText('0')
+    main_window.tb_o.setText('0')
+    if main_window.rb_cpu.isChecked():
+        main_window.tb_another.setText('O (CPU)')
+    else:
+        main_window.tb_another.setText('O (RIVAL)')
+    reset_page()
 
 app = QApplication(sys.argv)
-
-player = 1
 
 loader = QUiLoader()
 main_window = loader.load('main.ui')
 main_window.show()
+
+loader_about = QUiLoader()
+about_window = loader_about.load('about.ui')
+
+def about():
+    about_window.show()
+
+def about_close():
+    about_window.close()
 
 buttons = [[main_window.btn1, main_window.btn2, main_window.btn3],
            [main_window.btn4, main_window.btn5, main_window.btn6],
@@ -95,5 +154,12 @@ buttons = [[main_window.btn1, main_window.btn2, main_window.btn3],
 for row in range(3):
     for col in range(3):
         buttons[row][col].clicked.connect(partial(play, row, col))
+main_window.btn_newgame.clicked.connect(reset_page)
+main_window.rb_cpu.toggled.connect(reset_game)
+main_window.rb_player.toggled.connect(reset_game)
+main_window.btn_about.clicked.connect(about)
+about_window.btn_ok.clicked.connect(about_close)
+
+reset_page()
 
 app.exec()
---
+++ PyLearnGUITicTacToe/ui_about.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'about.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTextEdit,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(322, 308)
+        MainWindow.setMinimumSize(QSize(322, 308))
+        MainWindow.setMaximumSize(QSize(322, 308))
+        MainWindow.setMouseTracking(False)
+        MainWindow.setTabletTracking(False)
+        MainWindow.setAcceptDrops(False)
+        MainWindow.setStyleSheet(u"background-color: white; border: none;")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.textEdit = QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(30, 20, 241, 221))
+        font = QFont()
+        font.setPointSize(15)
+        self.textEdit.setFont(font)
+        self.textEdit.setStyleSheet(u"background-color: transparent; border: none;")
+        self.textEdit.setReadOnly(True)
+        self.btn_ok = QPushButton(self.centralwidget)
+        self.btn_ok.setObjectName(u"btn_ok")
+        self.btn_ok.setGeometry(QRect(210, 230, 71, 31))
+        self.btn_ok.setFont(font)
+        self.btn_ok.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(150, 150, 150, 255), stop:1 rgba(170, 170, 170, 255))\n"
+"")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(170, 10, 91, 81))
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setPixmap(QPixmap(u"tic tac toe.jpeg"))
+        self.label.setScaledContents(True)
+        self.label.setAlignment(Qt.AlignCenter)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 322, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.textEdit.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Segoe UI'; font-size:15pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:700;\">Tic Tac Toe</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-weight:700;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">version 6.4.2</p>\n"
+"<p style=\" margin-to"
+                        "p:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Tic Tac Toe is one of the classic game. This app is developed by Mohammad Saberian in February 2023. The program source is available on GitHub </span><a href=\"https://github.com/MSaberian/PyLearnGUI/tree/main/PyLearnGUITicTacToe\"><span style=\" font-size:12pt; text-decoration: underline; color:#0000ff;\">github link</span></a><span style=\" font-size:12pt;\">.</span></p></body></html>", None))
+        self.btn_ok.setText(QCoreApplication.translate("MainWindow", u"ok", None))
+        self.label.setText("")
+    # retranslateUi
+
