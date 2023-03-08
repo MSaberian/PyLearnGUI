5960daf44850f15657ea59f867c6dc91a91e41be
MSaberian <mo.saberian@yahoo.com>
2023-03-08 18:54:24 +0330

    check function

--- PyLearnGUISudocu/main.py
+++ PyLearnGUISudocu/main.py
@@ -9,10 +9,6 @@ from main_window import Ui_MainWindow
 from sudoku import Sudoku
 import qdarktheme
 
-# customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-# customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-# sys.argv += ['-platform', 'windows:darkmode=2']
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -25,12 +21,15 @@ class MainWindow(QMainWindow):
         self.ui.actionlight.triggered.connect(partial(self.setColor,'light','black'))
         self.ui.actiondark.triggered.connect(partial(self.setColor,'black'))
         self.line_edits = [[None for i in range(9)]for j in range(9)]
+        self.editableCells = []
         self.picking_boxes()
         self.new_game()
+        self.them = 'light'
         self.checkable = True
         qdarktheme.setup_theme("light")
 
     def setColor(self, color, textColor = 'black'):
+        self.them = color
         if color == 'black':
             qdarktheme.setup_theme("dark")
         else:
@@ -39,10 +38,18 @@ class MainWindow(QMainWindow):
             for i in range(9):
                 for j in range(9):
                     self.line_edits[i][j].setStyleSheet(f'color: {textColor}')
+        # self.check()
+    
+    def resetColor(self):
+        self.setStyleSheet('')
+        for i in range(9):
+            for j in range(9):
+                self.line_edits[i][j].setStyleSheet('')
 
     def open_file(self):
         file_path = QFileDialog.getOpenFileName(self, 'Open file...')[0]
         # print(file_path)
+        self.editableCells.clear()
         f = open(file_path, 'r')
         big_text = f.read()
         rows = big_text.split('\n')
@@ -56,6 +63,7 @@ class MainWindow(QMainWindow):
         for i in range(9):
             for j in range(9):
                 if puzzle_board[i][j] != 0:
+                    self.editableCells.append([i,j])
                     self.line_edits[i][j].setText(str(puzzle_board[i][j]))
                     self.line_edits[i][j].setReadOnly(True)
                 else:
@@ -63,20 +71,57 @@ class MainWindow(QMainWindow):
                     self.line_edits[i][j].setReadOnly(False)
         self.checkable = True
 
-    def check(self):
-        for i in range(0, 9):
-            for j in range(0, 9):
-                number1 = self.line_edits[i][0].text()
-                number2 = self.line_edits[i][j].text()
-                if number1 == number2:
-                    print('❌')
-                    return False
+    def setCellFalse(self,row,col):
+        if [row,col] in self.editableCells:
+            self.line_edits[row][col].setStyleSheet('background-color: pink')
+        else:
+            self.line_edits[row][col].setStyleSheet('background-color: red')
+
+    def check(self,i=-1,j=-1):
+        output = True
+        self.resetColor()
+        for row in range(9):
+            for col in range(9):
+                number1 = self.line_edits[row][col].text()
+                for row9 in range(row+1,9):
+                    number2 = self.line_edits[row9][col].text()
+                    if number1 == number2 and number2 != '':
+                        self.setCellFalse(row,col)
+                        self.setCellFalse(row9,col)
+                        # print(f'❌ row: {row}, col: {col}, row9: {row9}, number1: {number1}, number2: {number2}')
+                        output = False
+
+                for col9 in range(col+1,9):
+                    number2 = self.line_edits[row][col9].text()
+                    if number1 == number2 and number2 != '':
+                        self.setCellFalse(row,col)
+                        self.setCellFalse(row,col9)
+                        # print(f'❌ row: {row}, col: {col}, row9: {row9}, number1: {number1}, number2: {number2}')
+                        output = False
+
+        for n in range(0, 9, 3):
+            for m in range(0, 9, 3):
+                for row in range(n, n + 3):
+                    for col in range(m, m + 3):
+                        number1 = self.line_edits[row][col].text()
+                        for row3 in range(n, n + 3):
+                            for col3 in range(m, m +3):
+                                number2 = self.line_edits[row3][col3].text()
+                                if number1 == number2 and number2 != '' and not (row == row3 and col == col3):
+                                    self.setCellFalse(row,col)
+                                    self.setCellFalse(row3,col3)
+                                    print(f'❌ row: {row}, col: {col}, row3: {row3}, col3: {col3},number1: {number1}, number2: {number2}')
+                                    output = False
+
+
+
+        return output
 
     def validation(self, i, j, text):
         if self.checkable:
             if text not in ['1','2','3','4','5','6','7','8','9']:
                 self.line_edits[i][j].setText('')
-            if self.check():
+            if self.check(i,j):
                 msg_box = ...
 
     def picking_boxes(self, color = 'black'):
@@ -94,10 +139,12 @@ class MainWindow(QMainWindow):
 
     def new_game(self):
         self.checkable = False
+        self.editableCells.clear()
         puzzle = Sudoku(3, seed= random.randint(1, 1000)).difficulty(0.5)
         for i in range(9):
             for j in range(9):
                 if puzzle.board[i][j] is not None:
+                    self.editableCells.append([i,j])
                     self.line_edits[i][j].setText(str(puzzle.board[i][j]))
                     self.line_edits[i][j].setReadOnly(True)
                 else:
